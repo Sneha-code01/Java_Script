6f1261a7fc71e20194108260321c2d6f3ab3471c
Sneha-code01 <shrutisneha31@gmail.com>
2025-07-03 01:14:02 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,16 @@
+let n = 89;
+for (let i = 0; i < n; i++) {
+  console.log("apna gumla");    
+} 
+
+//user input
+let gamenum = 25;
+let usernum = prompt("Guess the game number:");
+
+while(usernum !== gamenum)
+{
+  usernum = prompt("you entered wrong guess again")
+  //game
+}
+
+console.log("cogo!! u entered write number")
