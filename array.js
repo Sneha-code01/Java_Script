cdbe9c4fc00737a2cad11de49f86c1a5337a2393
Sneha-code01 <shrutisneha31@gmail.com>
2025-07-02 23:34:29 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,7 @@
+let marks = [56,67,78,90];
+console.log(marks);
+let names = ["sneha", "shruti", "gungun", "choudhary", "dhelki", "dhela"];
+for (let index = 0; index < names.length; index++) {
+  console.log(names[index]);
+    
+}
