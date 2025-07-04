546bea9b2cd32f369688b245b011b956748c2941
Sneha-code01 <shrutisneha31@gmail.com>
2025-07-04 18:19:49 +0530

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,11 @@
+let x =4;
+if (x % 2 === 0) {
+    console.log("even"); 
+} else {
+    console.log("odd");
+}
+//ternary operator
+let age = 89;
+let check = age >=18 ? "adult" : "not adult";
+console.log(check);
+  
